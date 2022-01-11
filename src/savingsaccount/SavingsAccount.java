1e07df003a9e9b1b75c286cdacb772d9250838d0
hquantz <hunter.quantz9302@mail.davistech.edu>
2022-01-10 20:45:54 -0700

    Created initial file and Main method

---
+++ src/savingsaccount/SavingsAccount.java
@@ -0,0 +1,22 @@
+/*
+ * Dream.java
+ * Hunter Quantz
+ * 
+ * This program prints
+ */
+package savingsaccount;
+
+/**
+ *
+ * @author hlqua
+ */
+public class SavingsAccount {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
