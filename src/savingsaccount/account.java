d59f75d2af474cce3936f2bd347c793fe761a5f7
hquantz <hunter.quantz9302@mail.davistech.edu>
2022-01-11 18:23:53 -0700

    Added some comments and cleaned up code in Account.java

--- src/savingsaccount/account.java
+++ src/savingsaccount/account.java
@@ -10,22 +10,22 @@ public class account {
 
     private double balance = 0.0; //stores the current balance of the checking account
     private double interestRate; //stores the interest rate
-    
-    public void setInterestRate(double interestRate){
+
+    public void setInterestRate(double interestRate) {
         this.interestRate = interestRate / 100.0;
-    }
-    
-    public void setBalance(double balance){
+    }//end setInterestRate
+
+    public void setBalance(double balance) {
         this.balance = balance;
-    }
-    
-    public double getBalance(){
+    }//end setBalance
+
+    public double getBalance() {
         return this.balance;
-    }
-    
-    public void growAccount(int cycles){
-        for (int i = 0; i < cycles; i++){
-        this.balance += (this.interestRate * this.balance);
-    }
-    }
+    }//end getBalance
+
+    public void growAccount(int cycles) {
+        for (int i = 0; i < cycles; i++) {
+            this.balance += (this.interestRate * this.balance);
+        }
+    }//end growAccount
 }
